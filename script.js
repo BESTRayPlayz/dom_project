2af77ad406d8fdd97175c19a9889da4a767b7582
BESTRayPlayz <ray.luo.chen@gmail.com>
2026-07-27 16:12:29 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Add your code to this file
+const body = document.querySelector("body");
+body.style.backgroundColor = 'darkblue';
+
+
+const h1 = document.querySelector('h1');
+h1.style.textDecoration = 'underline';
+
+
+const firstParagraph = document.querySelector('.introduction');
+firstParagraph.style.textAlign = 'center';
+
+const images = document.getElementsByClassName('pokemon_images');
+
+for(let i=0; i < images.length; i++) {
+images[i].style.border = '5px dotted yellow';
+}
+
+
+const names = document.querySelectorAll('.pokemon_names');
+
+for(let i=0; i < names.length; i++) {
+    names[i].style.color = 'yellow';
+}
+
+
+const footer = document.querySelector('footer');
+footer.style.textDecoration = 'underline overline';
+
+
+
